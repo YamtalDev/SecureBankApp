814325c718638eadddcf3b9c92d2ed24c18571bf
YamtalDev <tal.aharon.work@gmail.com>
2024-09-18 13:06:07 +0300

    removing java scelaton to move to nodejs

--- src/test/java/com/akamai/HackerNews/IntegrationTests/UpdateNewsPostEndpointTest.java
+++
@@ -1,8 +0,0 @@
-package com.akamai.HackerNews.IntegrationTests;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class UpdateNewsPostEndpointTest {
-    
-}
